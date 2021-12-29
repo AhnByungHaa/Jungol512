e74cd0c347851b681a3748019d5cfcce3e711086
ahnbh <ahnbh@DESKTOP-38GH1PM>
2021-12-29 11:09:49 +0900

    512

---
+++ project_211228/src/project_211228/Jungol512.java
@@ -0,0 +1,11 @@
+package project_211228;
+
+public class Main {
+	 
+    public static void main(String[] args) {
+        int a=49;
+        float b=0.2683f;
+        float c=a*b;
+        System.out.printf(a+" * "+b+"00 = "+c+"00");
+    }
+}
